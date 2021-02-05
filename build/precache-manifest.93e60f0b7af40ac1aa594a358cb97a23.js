8f0725aeff17f793e66382881e88a7a8ff06e020
prasanth <prasathraj94@gmail.com>
2021-02-05 13:03:03 +0530

    build added

---
+++ build/precache-manifest.93e60f0b7af40ac1aa594a358cb97a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f2b86153e8e0a31f3c1",
+    "url": "/static/css/main.eb9d9eff.chunk.css"
+  },
+  {
+    "revision": "5f2b86153e8e0a31f3c1",
+    "url": "/static/js/main.5f2b8615.chunk.js"
+  },
+  {
+    "revision": "9de99ac9323795ca5d4e",
+    "url": "/static/css/1.3670d1de.chunk.css"
+  },
+  {
+    "revision": "9de99ac9323795ca5d4e",
+    "url": "/static/js/1.9de99ac9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0156967e522c5fc7a3d212c924fc7751",
+    "url": "/index.html"
+  }
+];
